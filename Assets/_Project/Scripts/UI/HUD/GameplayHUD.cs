a0f3e501679e7f944ad518b5d4b343f980aa4d67
CGA <113306447+Cgaz275@users.noreply.github.com>
2026-08-16 18:05:15 +0700

    Update Score

--- Assets/_Project/Scripts/UI/HUD/GameplayHUD.cs
+++ Assets/_Project/Scripts/UI/HUD/GameplayHUD.cs
@@ -1,26 +1,31 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayHUD : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI highScoreText;
+    [SerializeField] private Image timeFill;
 
     private void OnEnable()
     {
         GameManager.OnScoreChanged += UpdateScore;
         GameManager.OnHighScoreChanged += UpdateHighScore;
+        GameManager.OnTimeChanged += UpdateTime;
 
         if (GameManager.Instance == null) return;
 
         UpdateScore(GameManager.Instance.CurrentScore);
         UpdateHighScore(GameManager.Instance.HighScore);
+        UpdateTime(GameManager.Instance.CurrentTime, GameManager.Instance.Config.maxTime);
     }
 
     private void OnDisable()
     {
         GameManager.OnScoreChanged -= UpdateScore;
         GameManager.OnHighScoreChanged -= UpdateHighScore;
+        GameManager.OnTimeChanged -= UpdateTime;
     }
 
     private void UpdateScore(int score)
@@ -36,6 +41,14 @@ public class GameplayHUD : MonoBehaviour
         if (highScoreText != null)
         {
             highScoreText.text = highScore.ToString();
+        }
+    }
+
+    private void UpdateTime(float currentTime, float maxTime)
+    {
+        if (timeFill != null)
+        {
+            timeFill.fillAmount = Mathf.Clamp01(currentTime / maxTime);
         }
     }
 }
